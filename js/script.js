596d55b837eaf7203179bf370321b199692c7318
Tatiana Panteleeva <tatiana.panteleeva2018@gmail.com>
2021-02-17 13:08:59 +1100

    update form

--- js/script.js
+++ js/script.js
@@ -1,29 +1,61 @@
-var link = document.querySelector(".btn-form");
-    var popup = document.querySelector(".modal-reservation");
-	let plus = popup.querySelectorAll(".plus");
-	let minus = popup.querySelectorAll(".minus");
-	var form = popup.querySelector("form");
+const link = document.querySelector(".btn-form");
+const popup = document.querySelector(".modal-reservation");
+const plusBtnAdults = popup.querySelector(".plus-btn-adults");
+const minusBtnAdults = popup.querySelector(".minus-btn-adults");
+const plusBtnChildren = popup.querySelector(".plus-btn-children");
+const minusBtnChildren = popup.querySelector(".minus-btn-children");
+const form = popup.querySelector("form");
+const displayAdults = popup.querySelector("#number-of-adults");
+const displayChildren = popup.querySelector("input.number-of-children");
 
 
-	let counter = 0;
-	var showadults = popup.querySelector("label.number-of-adults");
-	
-
-   link.addEventListener('click', function (evt) {
-    evt.preventDefault();
-    popup.classList.toggle('modal-show');
-	});
-
-	plus[0].addEventListener("click", function (evt) {
-    evt.preventDefault();
-});
-	plus[1].addEventListener("click", function (evt) {
-    evt.preventDefault();
+//toggle popup
+link.addEventListener('click', function (evt) {
+	evt.preventDefault();
+	popup.classList.toggle('modal-show');
 });
 
-	minus[0].addEventListener("click", function (evt) {
-    evt.preventDefault();
-	});
-	minus[1].addEventListener("click", function (evt) {
-    evt.preventDefault();
+window.addEventListener("keydown", function (evt) {
+	if (evt === 27) {
+		if (popup.classList.contains("modal-show")) {
+			popup.classList.remove("modal-show");
+		}
+	}
 });
+
+
+//popup form 
+plusBtnAdults.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	plus();
+});
+
+minusBtnAdults.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	minus();
+});
+
+plusBtnChildren.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	plus();
+});
+
+minusBtnChildren.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	minus();
+});
+
+let score = 0;
+function plus() {
+	score += 1;
+	return displayAdults.value = score;
+}
+
+function minus() {
+	if (displayAdults.value <= 0) {
+		return displayAdults.value = 0;
+	} else {
+		score -= 1;
+		return displayAdults.value = score;
+	}
+}
